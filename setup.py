93eeb657a60a22803254fab2577cf47780df20e2
He <xiaotonghe@newbanker.cn>
2021-01-19 15:13:41 +0800

    test pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='jujube_pill',
+    version='0.0.1',
+    author='xlzd',
+    author_email='what@the.f*ck',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'吃枣药丸',
+    packages=['jujube_pill'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'jujube=jujube_pill:jujube',
+            'pill=jujube_pill:pill'
+        ]
+    }
+)
